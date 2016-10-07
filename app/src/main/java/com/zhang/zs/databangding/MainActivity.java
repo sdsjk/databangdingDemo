9267febb5bb721c7f3dfa15a11fee5c4ed5fc5bf
zhang <154789953@qq.com>
2016-10-07 17:41:43 +0800

    Initial commit　哈哈

---
+++ app/src/main/java/com/zhang/zs/databangding/MainActivity.java
@@ -0,0 +1,18 @@
+package com.zhang.zs.databangding;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zhang.zs.databangding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setStu(new Student("TOM"));
+    }
+}
---
+++ app/src/main/java/com/zhang/zs/databangding/Student.java
@@ -0,0 +1,30 @@
+package com.zhang.zs.databangding;
+
+/**
+ * Created by zs on 2016/8/17.
+ */
+public class Student {
+
+    private String name;
+
+
+    public Student() {
+    }
+
+    public Student(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+}
