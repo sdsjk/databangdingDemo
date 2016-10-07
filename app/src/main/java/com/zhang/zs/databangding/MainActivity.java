b94ef3e19cfafcce995d81ea2ad42afc8ed511b5
zhang <154789953@qq.com>
2016-10-07 18:07:03 +0800

    添加日志

--- app/src/main/java/com/zhang/zs/databangding/MainActivity.java
+++ app/src/main/java/com/zhang/zs/databangding/MainActivity.java
@@ -3,6 +3,7 @@ package com.zhang.zs.databangding;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.zhang.zs.databangding.databinding.ActivityMainBinding;
 
@@ -14,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.setStu(new Student("TOM"));
+        Log.e("TAG","1111");
     }
 }
